eb35dd8d29b9d0c823b57e7cd991789c46fb405e
André gois <andre.s.gois3@gmail.com>
2020-12-13 21:19:47 -0300

    Iniciando com promise

---
+++ Modulo10- Programação assíncrona - Promises e AsyncAwait/app.js
@@ -0,0 +1,70 @@
+
+// já é assincrono
+setTimeout(() => {
+  console.log('Exibe depois de 2s');
+},2000);
+
+/*function enviarEmail(corpo, para){
+  setTimeout(() => {
+    console.log(`
+      Para: ${para}
+      ----------------------------
+      ${corpo}
+      ----------------------------
+      De: Teest
+    `);
+  },2000);
+}
+
+console.log('Inicio do envio');
+enviarEmail("Oi, tudo bem","andre@emial.com");
+console.log('Enmail enviado');
+console.log('Tudo OK');*/
+
+//---------------- Agora por callback
+/*function enviarEmail(corpo, para, callbalb){
+  setTimeout(() => {
+    console.log(`
+      Para: ${para}
+      ----------------------------
+      ${corpo}
+      ----------------------------
+      De: Teest
+    `);
+    callbalb();
+  },2000);
+}
+
+console.log('Inicio do envio');
+enviarEmail("Oi, tudo bem","andre@emial.com", () => {
+    console.log('Enmail enviado');
+    console.log('Tudo OK');
+});*/
+
+//---------------- Promise
+function enviarEmail(corpo, para){
+  return new Promise( (resolve, reject) => {
+    setTimeout( () => {
+      var deuErro = false;
+      console.log("Email enviado");
+
+      if(!deuErro){
+        resolve({time:6, to: "andre@email.com"});  // Promessa OK
+      }else{
+        reject("Não enviado");   // Foi mal, eu falhei :(
+      }
+    },4000);
+  })
+}
+
+enviarEmail("Olá mundo", "andre@email.com")
+      .then( ({time, to}) => {
+        console.log(`
+          Time: ${time}
+          ------------------------
+          To: ${to}
+        `);
+      }).catch( (err) => {
+        console.log("Error: " + err);
+      });
+      // Aula 12
